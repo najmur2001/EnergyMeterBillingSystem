7281d74f49eb66901be2c3df20172b357826ed40
Najmur Hossain Kayal <121518139+najmur2001@users.noreply.github.com>
2024-02-26 12:22:22 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,27 +1,3 @@
-// export default {
-//     JWT_SECRET: "rl2iT8N1m4aK2cJS5M/PS7PRAf0s5hF8FlRyPEeAItk=",
-//     EMAIL: "j28726217@gmail.com",
-//     PASSWORD: "fffgowxmtcteefsh",
-//     MONGODB_URI: "mongodb://localhost:27017/NAJMUR"
-// }
-
-
-
-// export default{
-//     JWT_SECRET : "1+ChUCAhUNQzFlL37pvQwoQ4ybJz2ZI8AL40Q0g+5Ug=",
-//     EMAIL: "muhammad66@ethereal.email",
-//     PASSWORD:"6B4ztq7Vg78REBRx9U"
-// }
-
-
-// export default{
-//     JWT_SECRET : "1+ChUCAhUNQzFlL37pvQwoQ4ybJz2ZI8AL40Q0g+5Ug=",
-//     EMAIL: "onlineonline11234@gmail.com",
-//     PASSWORD:"1ljal hbfc arux gluz"
-// }
-
-
-
 export default{
     JWT_SECRET : "1+ChUCAhUNQzFlL37pvQwoQ4ybJz2ZI8AL40Q0g+5Ug=",
     EMAIL: "najmur.extra@gmail.com",
